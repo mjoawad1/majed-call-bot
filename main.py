b617a61b25d4f550924188708e469188f01d5ec0
mjoawad1 <mjo.awad@gmail.com>
2025-07-15 15:34:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import time
+
+print("Majed Call Bot is running...")
+
+while True:
+    # This is a placeholder for the main logic
+    print("Checking market conditions...")
+    time.sleep(60 * 60 * 4)  # simulate 4-hour scan
